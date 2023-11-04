0b35561c8788fc8d13ff7ab41e5f14b338468f6b
jrrlucenafilho <jrrlucenafilho@gmail.com>
2023-11-03 22:18:03 -0300

    Added 'CensorLineBuffer function and tidied it up

--- pic_censor_bmp/pic_censor_bmp.asm
+++ pic_censor_bmp/pic_censor_bmp.asm
@@ -65,13 +65,107 @@ include \masm32\macros\macros.asm
     ;3 bytes/pixel multiplicados por 2160 pixels
     img_line_bytes_buffer db 6480 dup(0)
 
-    ;3-byte pixel Array p apontar pra cada byte de qualquer pixel
-    pixel_rgb_arr_ptr db 3 dup(0)
+    ;Counts the number of pixels that have been currently iterated through in the line buffer
+    ;So as to check when to start censoring and when to stop censoring the line buffer
+    buffer_painted_pixels_counter DWORD 0
+
+    ;Counts the height currently iterated by line buffer (in lines ofc)
+    y_pos_counter DWORD 0
 
 .code
-start:
-;;;Prompts de entrada;;;
+;;Censoring-line-buffer function
+;;CensorLineBuffer(img_line_bytes_buffer, coord_x, largura_censura)
+CensorLineBuffer:
+    ;Prologue
+    push ebp
+    mov ebp, esp
+    sub esp, 20 ;Reserva bytes para 5 vars locais de 4 bytes (5 vars * 4 bytes = 20 bytes)
 
+;;Estrutura da Pilha durante essa função
+    ;;Parameters:
+    ;DWORD PTR [ebp+8] = offset img_line_bytes_buffer
+    ;DWORD PTR [ebp+12] = coord_x
+    ;DWORD PTR [ebp+16] = largura_censura
+
+    ;Vars locais:
+    ;DWORD PTR [ebp-4] = buffer_painted_pixels_counter
+    ;DWORD PTR [ebp-8] = eax_initial_value   ;;These "reg_initial_value" will save the values of each reg before they were used in this function
+    ;DWORD PTR [ebp-12] = ebx_initial_value  ;;Since this is a windows program, and so should follow the Callee Clean-up convention
+    ;DWORD PTR [ebp-16] = ecx_initial_value  ;;Which means it just needs to preserve used regs
+    ;DWORD PTR [ebp-20] = edx_initial_value
+
+    ;Salva o valor inicial de eax na pilha
+    ;(Deixa um "buraco" na pilha pois tenho que usar o eax para salvar buffer_painted_pixels_counter como a primeira var local)
+    ;(Portanto, tenho que salvar eax antes de usá-lo, e fazer assim me permite deixar todos os "reg_initial_value"'s juntos no topo da pilha)
+    mov DWORD PTR [ebp-8], eax
+
+    ;Movendo buffer_painted_pixels_counter para a pilha como var local (grava zero mesmo, no início)
+    mov eax, buffer_painted_pixels_counter
+    mov DWORD PTR [ebp-4], eax
+
+    ;Empilhando os outros valores dos regs usados antes de usá-los
+    push ebx
+    push ecx
+    push edx
+
+    ;First, save largura in ecx
+    mov ecx, DWORD PTR [ebp+16]
+
+    ;;Iterar pela pelo buffer
+    ;if(pixel_atual >= coord_x && pixel_atual <= (coord_x + largura)){ pintar de preto }
+    ;move ptr to x_coord in line buffer, using lea (load effective address)
+    ;move x_coord in the line buffer to ebx (to be used as an index)
+    ;But first, I need to multiply x_coord by 3, since x_coord is meant as in pixels
+    ;And i need to convert it to bytes
+    
+    ;So I just load it to ebx
+    mov ebx, [ebp+12]
+
+    ;Then multiply ebx by 3 (1 pixel = 3 bytes)
+    imul ebx, 3
+
+    ;Calculate the address of the desired byte in line buffer
+    lea eax, img_line_bytes_buffer[ebx] 
+    
+    ;Now eax points to x_coord's pixel first byte, just need to paint it black until (coord_x + largura)
+paint_it_black_label:
+    ;First, set the first 3 bytes to 0 (sets the first pixel in line buffer to black)
+    mov BYTE PTR [eax], 0    ;Set blue byte to 0
+    mov BYTE PTR [eax+1], 0  ;Set green byte to 0
+    mov BYTE PTR [eax+2], 0  ;Set red byte to 0
+
+    ;Now we need to make eax point to (eax + 3) (next pixel's blue byte)
+    add eax, 3 ;Now eax points to the next pixel's blue byte
+
+    ;Increase the painted_pixels_counter by 1 for each pixel painted black
+    inc DWORD PTR [ebp-4]
+
+    ;;Setting up the stopping condition
+    ;;Compares if buffer_painted_pixels_counter == (largura-1)
+    ;First, loads buffer_painted_pixels_counter to edx
+    mov edx, DWORD PTR [ebp-4]
+
+    ;Compares if buffer_painted_pixels_counter (in edx) == (largura-1) (in ecx)
+    cmp edx, ecx
+
+    ;If they're different, it means it hasn't painted the whole section asked in line buffer to black
+    ;So jump back to label
+    jne paint_it_black_label
+    ;If they're equal, then the correct width has been painted and the function is finished
+
+    ;Epilogue
+    ;Popping os regs usados, para recuperarem seus valores anteriores ao seu uso na função (Callee-Cleanup convention)
+    pop edx
+    pop ecx
+    pop ebx
+    mov eax, DWORD PTR [ebp-8]
+
+    mov esp, ebp
+    pop ebp
+    ret 12  ;Desempilha os 3 params (4 bytes * 3 params = 12 bytes)
+
+start:
+;;Prompts de entrada
     ;Setup dos Handles
     invoke GetStdHandle, STD_INPUT_HANDLE
     mov inputHandle, eax
@@ -108,9 +202,8 @@ start:
     ;Entrada da altura (como str)
     invoke ReadConsole, inputHandle, addr altura_str, sizeof altura_str, addr console_count, NULL
 
-;;;Remoção do '/n' em coord_x, coord_y, altura e largura;;;
-
-    ;Tirar CR da str nome_do_arquivo
+;;Remoção do '/n' em coord_x, coord_y, altura e largura
+    ;Tira CR da str nome_do_arquivo
     mov esi, offset nome_arquivo_str ;Salva o ptr da string
 proximo_label_nome_arquivo_str:
     mov al, [esi]   ;Move char da iter atual pra al (8-bit)
@@ -125,7 +218,7 @@ proximo_label_nome_arquivo_str:
     xor esi, esi
     xor al, al
 
-    ;Tirar CR da coord_x
+    ;Tira CR da coord_x
     mov esi, offset coord_x_str ;Salva o ptr da string
 proximo_label_coord_x_str:
     mov al, [esi]
@@ -139,7 +232,7 @@ proximo_label_coord_x_str:
     xor esi, esi
     xor al, al
 
-   ;Tirar CR da coord_y
+   ;Tira CR da coord_y
     mov esi, offset coord_y_str
 proximo_label_coord_y_str:
     mov al, [esi]
@@ -153,7 +246,7 @@ proximo_label_coord_y_str:
     xor esi, esi
     xor al, al
 
-    ;Tirar CR da largura
+    ;Tira CR da largura
     mov esi, offset largura_str
 proximo_label_largura_str:
     mov al, [esi]
@@ -167,7 +260,7 @@ proximo_label_largura_str:
     xor esi, esi
     xor al, al
 
-    ;Tirar CR da altura
+    ;Tira CR da altura
     mov esi, offset altura_str
 proximo_label_altura_str:
     mov al, [esi]
@@ -181,13 +274,11 @@ proximo_label_altura_str:
     xor esi, esi
     xor al, al
 
-;;;Conversões de coord_x, coord_y, altura e largura para dword (4 bytes);;;
-
+;;Conversões de coord_x, coord_y e largura para dword (4 bytes)
     ;Limpar eax que vai armazenar os valores numéricos
     xor eax, eax
 
-    ;Converte as strings em dwords (por meio de eax)
-    ;e as armazena nas variáveis
+    ;Converte as strings em dwords (por meio de eax) e as armazena nas variáveis
     invoke atodw, addr coord_x_str
     mov coord_x, eax
 
@@ -216,7 +307,7 @@ proximo_label_altura_str:
     ;Ler os últimos 28 bytes do header
     invoke ReadFile, input_file_handle, addr header_section2, 32, addr effectively_read_bytes, NULL
 
-;;Escrita para o header do arquivo de censor
+;;Escrita para o header do arquivo de output
     ;Criação do arquivo de output
     invoke CreateFile, addr output_file_name, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
     mov output_file_handle, eax
@@ -232,19 +323,27 @@ proximo_label_altura_str:
     invoke WriteFile, output_file_handle, addr header_section2, 32, addr effectively_written_bytes, NULL
 
 ;;Lendo os bytes dos pixels da img de entrada e escrevendo na de saída
-
-    ;1. Lê e salva no buffer
-    ;2. Escreve a linha na img de saída
+    ;1. Lê próx linha e salva no buffer
+    ;2. Escreve a linha do buffer na img de saída
     ;3. se não chegar em EOF, volta pro 1.
 
     ;Multiplica a largura por 3 e guarda em img_line_byte_num
-    mov ecx, arquivo_largura
-    imul ecx, 3
-    mov img_line_byte_num, ecx
+    mov ebx, arquivo_largura
+    imul ebx, 3
+    mov img_line_byte_num, ebx
 
 copy_img_line_label:
     ;Lê bytes da img de input até a (largura da imagem * 3) e salva no buffer de linha
     invoke ReadFile, input_file_handle, addr img_line_bytes_buffer, img_line_byte_num, addr effectively_read_bytes, NULL
+
+;;CensorLineBuffer function call here
+    ;Should only call this function if y_pos_counter is between y_pos and y_pos + altura
+
+    ;Pushing params and calling function
+    push largura_censura
+    push coord_x
+    push offset img_line_bytes_buffer
+    call CensorLineBuffer
 
     ;Escreve na img de output a linha que está no buffer até a (largura da imagem * 3)
     invoke WriteFile, output_file_handle, addr img_line_bytes_buffer, img_line_byte_num, addr effectively_written_bytes, NULL
@@ -261,21 +360,6 @@ copy_img_line_label:
     invoke ExitProcess, 0
 end start
 
-;;Censoring-line-buffer function
-;;CensorLineBuffer(img_line_bytes_buffer, coord_x, largura_censura)
-CensorLineBuffer:
-    ;Prologue
-    push ebp
-    mov ebp, esp
-    sub esp, 8
-
-
-
-    ;Epilogue
-    mov esp, ebp
-    pop ebp
-    ret 4
-
 ;;;Entrada
 ;Prompt e entrada do nome do arquivo - 
 ;Prompt de entrada de posição x - 
@@ -283,11 +367,11 @@ CensorLineBuffer:
 ;Prompt de largura -
 ;Prompt de altura -
 
-;;Remover /n da str de numeros -
-;;Converter posicoes x e y e largura e altura em dword -
+;Remover /n da str de numeros -
+;Converter posicoes x e y e largura e altura em dword -
 
 ;;;Manipulaçao de arquivo
-;;Abrir arquivo de input e ouput -
-;;Ler header do arq. de input e escrever no arq. de output -
-;;Copiar pixels da imagem em si -
-;;Add censura do arquivo (função(addr_array, coordX, largura_da_censura)
+;Abrir arquivo de input e ouput -
+;Ler header do arq. de input e escrever no arq. de output -
+;Copiar pixels da imagem em si -
+;Add censura do arquivo (função(addr_array, coordX, largura_da_censura
